dcae2850286a2694843500629f488373372b2f83
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2025-01-02 19:55:17 +0530

    Initial Commit

---
+++ Weather App SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App SwiftUI
+//
+//  Created by utkarsh mishra on 02/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather App SwiftUI/Weather_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_SwiftUIApp.swift
+//  Weather App SwiftUI
+//
+//  Created by utkarsh mishra on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
